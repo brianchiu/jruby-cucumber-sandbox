b828eae65e8f24ab8171e62d20cb0d006a666600
Brian Chiu <brian.chiu@liferay.com>
2017-08-30 18:25:52 -0700

    Not needed

--- features/step_definitions/base_steps.rb
+++ features/step_definitions/base_steps.rb
@@ -1,13 +1,13 @@
 Given(/^I click the "([^"]*)" button$/) do |button|
-	page.click_button button
+	click_button button
 end
 
 Given(/^I click the "([^"]*)" link$/) do |link|
-	page.click_link link
+	click_link link
 end
 
 Given(/^I enter "([^"]*)" in the "([^"]*)" field$/) do |text, field|
-	page.fill_in field, with: text
+	fill_in field, with: text
 end
 
 Given(/^I navigate to "([^"]*)"$/) do |url|
